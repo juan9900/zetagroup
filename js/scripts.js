5bb435a289d0fd956d3ddf723946adf5922eee76
juan9900 <juanluislauretta@gmail.com>
2023-01-12 11:33:44 -0400

    added product images carousel system with more products

--- js/scripts.js
+++ js/scripts.js
@@ -464,6 +464,37 @@ $(document).ready(()=>{
 
     $('.btn-get-product').each(function(index){
         $(this).on('click',function(){
+            const carouselIndicators = $('.carousel-indicators');
+            const myCarouselElement = $('#carousel-more-product');
+            const carouselProductsImages = $('.more-product-inner');
+            //Set the carousel back to position 0 when reopening the modal
+            const carousel = new bootstrap.Carousel(myCarouselElement).to(0);
+            $(carouselIndicators).empty();
+            const product = $(this).attr('data-bs-product').replace(' ','-');
+            const imagesQty = parseInt($(this).attr('data-bs-images'));
+
+            // Take the value of data-bs-images in each product, the value must be an integer
+            // this will create X number of buttons for each image of the product (data-bs-images)
+            for(let i = 0; i< imagesQty; i++){
+                const newButton = `<button class="${i===0 ? 'active' : '' }" aria-current="${i===0?true:false}" type="button" data-bs-target="#carousel-more-product" data-bs-slide-to="${i}" aria-label="Slide ${i+1}"></button>`;
+                
+                $(carouselIndicators).append(newButton);
+
+            }
+
+            //Generate images
+            $(carouselProductsImages).empty();
+            for(let i = 0; i<imagesQty; i++){
+                $(carouselProductsImages).append(`
+                <div class="carousel-item ${i === 0 ? 'active' : ''}">
+                    <img src="/assets/img/products/${product}-0${i+1}.png" class="d-block more-product-img" alt="...">
+                </div>`)
+            }
+            // $('.more-product-img').each(function(index, element){
+            //     // Image's name must match the data-bs-product attribute
+            //     // ex: data-bs-product: Cilindro superficial / image name: cilindro-superficial-01
+            //     $(this).attr('src',`/assets/img/products/${product}-0${index+1}.png`);
+            // })
             $('#formInfoProduct').val($(this).attr('data-bs-product'));
         })
     })
